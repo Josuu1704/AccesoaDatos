4d2c11cf102de75cd15b8d5e1f87dec0eeeb8e61
Josue <jmzo17.jz@gmail.com>
2025-09-29 14:14:48 +0200

    ejercicioFunciones

---
+++ T1/Sentencias/ejercicios/ej5.py
@@ -0,0 +1,28 @@
+"""
+Construir un programa que gestione personajes de
+Marvel usando funciones y manejo de excepciones.
+
+- Crea un diccionario vacio llamado marvel.
+- Implementa las siguientes funciones con manejo
+    de excepciones con raise cuando corresponda.
+
+    - si alguien introduce como nombre
+    un número arrojamos una excepción (raise)
+    porque tiene que ser string.
+    - Si el personaje existe arrojamos una excepción de
+    KeyError.
+
+- Implementa otra función para buscar un personaje.
+    - Si existe, imprime el personaje.
+    - Si no existe, arrojamos una excepción de KeyError.
+
+- Implementar otra función para obtener (con return), el
+    personaje con rating más alto.
+
+- Hacer un panel de control tipo:
+    - 1. Introducir usuario
+    - 2. Comprobar si existe
+    - 3. Ver personaje con mayor puntuación
+    - 4. Salir
+"""
+
